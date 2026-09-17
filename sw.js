1a21892ff1294d78d6659c4562aa155a9ee6d370
jmsnuwan-dotcom <jmsnuwan@gmail.com>
2026-09-17 14:47:42 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lumora-v15-mobile-live-fixed';
+const CACHE='lumora-v16-one-click-pwa';
 const ASSETS=['/manifest.json','/logo.png','/lumora-header.png','/icon-192.png','/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
